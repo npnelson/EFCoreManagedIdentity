4e31ac5bb2e5eb67b637a59d5c511972eb2250ca
Nicholas P Nelson <nnelson1@insight.rr.com>
2020-03-05 11:59:07 -0500

    Feature/initial (#1)

---
+++ NETToolBox.EFCoreManagedIdentity/ManagedIdentityConnectionInterceptor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NETToolBox.EFCoreManagedIdentity
+{
+    internal class ManagedIdentityConnectionInterceptor : DbConnectionInterceptor
+    {
+        private readonly string? _tenantId;
+        private readonly AzureServiceTokenProvider _tokenProvider;
+
+        public ManagedIdentityConnectionInterceptor(string? tenantID)
+        {
+            if (string.IsNullOrEmpty(tenantID))
+            {
+                _tenantId = null;
+            }
+
+            _tokenProvider = new AzureServiceTokenProvider();
+        }
+
+        public override async Task<InterceptionResult> ConnectionOpeningAsync(
+            DbConnection connection,
+            ConnectionEventData eventData,
+            InterceptionResult result,
+            CancellationToken cancellationToken = default)
+        {
+            // Have to cast DbConnection to SqlConnection
+            // AccessToken property does not exist on the base class
+            var sqlConnection = (SqlConnection)connection;
+            string accessToken = await GetAccessTokenAsync().ConfigureAwait(false);
+            sqlConnection.AccessToken = accessToken;
+
+            return result;
+        }
+
+        private async Task<string> GetAccessTokenAsync()
+        {
+            string resource = "https://database.windows.net/";
+            return await _tokenProvider.GetAccessTokenAsync(resource, _tenantId).ConfigureAwait(false);
+        }
+    }
+}
---
+++ NETToolBox.EFCoreManagedIdentity/ManagedIdentityConnectionInterceptorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NETToolBox.EFCoreManagedIdentity;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class ManagedIdentityConnectionInterceptorExtensions
+    {
+        /// <summary>
+        /// UseManagedIdentity for this SqlConnection specifiying an optional tenantID taken from https://github.com/juunas11/Joonasw.ManagedIdentityDemos
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        /// <param name="tenantID"></param>
+        /// <returns></returns>
+        public static DbContextOptionsBuilder UseManagedIdentity([NotNull] this DbContextOptionsBuilder optionsBuilder, string? tenantID = null)
+        {
+            optionsBuilder.AddInterceptors(new ManagedIdentityConnectionInterceptor(tenantID));
+            return optionsBuilder;
+        }
+    }
+}
